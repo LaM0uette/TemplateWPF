de8829bd27e44470410920b91d4d0afbb9cb58a6
LaM0uette <lam0uette.dev@gmail.com>
2022-04-26 09:45:45 +0200

    creation view button

---
+++ Views/ButtonView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace TemplateWPF.Views;
+
+public partial class ButtonView : UserControl
+{
+    public ButtonView()
+    {
+        InitializeComponent();
+    }
+}
