a00203211f1849624e9bdfaa7cdfcbfb709fab46
LaM0uette <lam0uette.dev@gmail.com>
2022-04-26 10:53:46 +0200

    demo views

---
+++ Views/DemoViews.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace TemplateWPF.Views;
+
+public partial class DemoViews : UserControl
+{
+    public DemoViews()
+    {
+        InitializeComponent();
+    }
+}
