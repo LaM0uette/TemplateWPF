23fbe899f189834f68aa0f8492e31ef5f4d99cfc
LaM0uette <lam0uette.dev@gmail.com>
2022-05-12 12:01:16 +0200

    creation des albeles styles

---
+++ Views/Demo/LabelView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TemplateWPF.Views.Demo;
+
+public partial class LabelView : UserControl
+{
+    public LabelView()
+    {
+        InitializeComponent();
+    }
+} 
